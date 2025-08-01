9907671b1dbb70584bbe13036518a396cb696d70
Barani B <barani2502@gmail.com>
2025-08-01 18:19:41 +0530

    Updated test.py

--- test.py
+++ test.py
@@ -8,11 +8,22 @@ model = YOLO("yolov8x.pt")  # You can change this to yolov8m.pt or yolov8l.pt
 model.conf = 0.5
 
 # Open webcam (0 = default camera)
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('input/input_video.mp4')
 
 if not cap.isOpened():
-    print("Error: Could not open webcam.")
+    print("Error: Could not open webcam or input video isn't present")
     exit()
+
+cv2.namedWindow("YOLOv8 - Webcam Object Detection", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("YOLOv8 - Webcam Object Detection", 1280, 720)
+
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+# Define the codec and create VideoWriter object
+out = cv2.VideoWriter("output/output_video.mp4",
+                      cv2.VideoWriter_fourcc(*"mp4v"), fps, (width, height))
 
 while True:
     ret, frame = cap.read()
@@ -34,13 +45,15 @@ while True:
     # Annotate detections
     annotated_frame = result.plot()
 
-    cv2.putText(annotated_frame, f"Objects Detected: {count}", (10, 30),
-                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.putText(annotated_frame, f"Objects Detected: {count}", (10, 50),
+                cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 5)
 
     cv2.imshow("YOLOv8 - Webcam Object Detection", annotated_frame)
+    out.write(annotated_frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 cap.release()
+out.release()
 cv2.destroyAllWindows()
